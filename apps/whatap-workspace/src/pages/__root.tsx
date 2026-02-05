ad3d03018a5f842422606af69c25a986e0cfefa0
pyeonzi20c <pyeonzi20c@gmail.com>
2026-02-05 11:49:40 +0900

    feat: root pate -> landing 으로 리다이렉트

--- apps/whatap-workspace/src/pages/__root.tsx
+++ apps/whatap-workspace/src/pages/__root.tsx
@@ -1,9 +1,16 @@
 import type { RouterContext } from '@app/context';
 import { RootLayout } from '@app/layouts';
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import { Outlet, createRootRouteWithContext, redirect } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRouteWithContext<RouterContext>()({
+  beforeLoad(ctx) {
+    if (ctx.location.pathname == '' || ctx.location.pathname == '/') {
+      throw redirect({
+        to: '/landing',
+      });
+    }
+  },
   component: RootComponent,
 });
 
